99a3f4a148b03d1fbdb3b97a18043b298da16051
Kevin <holmk21@gmail.com>
2023-10-31 06:27:33 -0400

    initial commit

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,18 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section montserrat">
+          <div className="hero--section--img">
+        <img  id="hero--img--main" className="heor---picture" src="./img/cool_guy.jpg" alt="Hero Section" />
+      </div>
+        <div className="hero--section--content">
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Become the person</span>{" "}
+            you were made to be</h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum?</p>
+            <button className="btn btn-primary hero--bnt">BOOK A CHAT</button>
+      </div>
+
+    </section>
+  );
+}
