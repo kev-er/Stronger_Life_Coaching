7695d8390ce887d960e6e56688d1dea50be66312
Kevin <holmk21@gmail.com>
2023-11-02 10:41:56 -0400

    fixing screen issue under 400px

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe() {
   return (
     <section id="AboutMe" className="about--section">
       <div className="about--section--img">
-        <img src="./img/about_me.png" alt="About Me" />
+        <img className="aboutMe--img" src="./img/about_me.png" alt="About Me" />
       </div>
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content content--box--rounded">
